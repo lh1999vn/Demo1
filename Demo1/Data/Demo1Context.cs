cd7c1827d3be78deef85e68dddeefbefc4c7ce8e
2023TU <tu@gmail.com>
2023-11-20 22:05:42 +0700

    first commit

---
+++ Demo1/Data/Demo1Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Demo1.Models;
+
+namespace Demo1.Data
+{
+    public class Demo1Context : DbContext
+    {
+        public Demo1Context (DbContextOptions<Demo1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Demo1.Models.Product> Product { get; set; } = default!;
+
+        public DbSet<Demo1.Models.Category>? Category { get; set; }
+
+        public DbSet<Demo1.Models.User>? User { get; set; }
+    }
+}
